1530e7e936573df256e9cf720a87e8f188c8fd61
Pushkar91949 <pushkar91949@gmail.com>
2023-04-01 14:46:49 +0530

    hi

---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta property="og:title" content="build by team Eklavya." key="title"/>
+        <meta property="og:description" content="Get your personalized fitness and diet plan specially designed by a specialized trained AI." key="description"/>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
